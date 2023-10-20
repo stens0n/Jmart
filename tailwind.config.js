3c35bd27a7b83de6f175d768189bb5b767a10996
stens0n <astenson010@gmail.com>
2023-10-20 17:12:42 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'baby-blue': '#89CFF0',
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
+
